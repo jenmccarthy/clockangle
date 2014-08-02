9f2607f4821fe4307331e905f73d68c86273b076
jennifermccarthy <jennifernward@gmail.com>
2014-08-02 19:09:57 +0000

    accounted for movement of the hour hand upon increasing minutes

--- lib/clockangle.rb
+++ lib/clockangle.rb
@@ -1,11 +1,11 @@
 def clockangle(time)
   time_array = time.split(":")
-  hour = time_array[0].to_i
-  minute = time_array[1].to_i
+  hour = time_array[0].to_f
+  minute = time_array[1].to_f
   if hour == 12
     hour = 0
   end
-  hour_degrees = hour * 30
+  hour_degrees = (hour * 30) + (minute / 2)
   min_degrees = minute * 6
   angle = hour_degrees - min_degrees
   result_angle = angle.abs
--- spec/clockangle_spec.rb
+++ spec/clockangle_spec.rb
@@ -5,4 +5,8 @@ describe "clockangle" do
   it "returns the degree angle of two hands of a clock on the hour" do
     expect(clockangle("12:00")).to eq (0)
   end
+  
+  it "returns the degree angle of two hands of a clock on any of the quarter hours" do
+    expect(clockangle("5:15")).to eq (67.5)
+  end
 end
